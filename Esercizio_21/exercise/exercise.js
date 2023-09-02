15f432e102c4937dbdc42a53b26e9db54fa65954
BlueBojack <curtis.ducasse1g@gmail.com>
2023-09-02 17:01:54 +0200

    Destructuring exercises

--- Esercizio_21/exercise/exercise.js
+++ Esercizio_21/exercise/exercise.js
@@ -2,9 +2,12 @@ let num1 = 10;
 let num2 = 20;
 console.log('Before swap: ', num1, num2); // Before swap: 10 20
 
+let [a,b,c,d] = [num1, num2]
+
 // Variable swapping
-let temp = num2;
-num2 = num1;
-num1 = temp;
+
+num2 = a;
+num1 = b;
 
 console.log('After swap: ', num1, num2); // After swap: 20 10
+
