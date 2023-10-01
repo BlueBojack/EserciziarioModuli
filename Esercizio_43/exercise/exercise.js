5611a53a7ec91be9c3269ecbf352cd8c40e56c6e
BlueBojack <curtis.ducasse1g@gmail.com>
2023-10-01 23:28:42 +0200

    file mancanti

--- Esercizio_43/exercise/exercise.js
+++ Esercizio_43/exercise/exercise.js
@@ -3,3 +3,15 @@ const user = {
   name: "John",
   age: 25,
 };
+
+function saveUserToLocalStorage() {
+  try {
+    const userJSON = JSON.stringify(user);
+    localStorage.setItem('user', userJSON);
+    console.log('User saved to localStorage.');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+saveUserToLocalStorage()
--- Esercizio_44/exercise/exercise.js
+++ Esercizio_44/exercise/exercise.js
@@ -3,3 +3,26 @@ const user = {
   name: "John",
   age: 25,
 };
+
+function saveUserToLocalStorage() {
+  try {
+    const userJSON = JSON.stringify(user);
+    localStorage.setItem('user', userJSON);
+    console.log('User saved to localStorage.');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function getUserFromLocalStorage() {
+  try {
+    const userJSON = localStorage.getItem('user');
+    const userObj = JSON.parse(userJSON);
+    console.log(userObj);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+saveUserToLocalStorage();
+getUserFromLocalStorage();
