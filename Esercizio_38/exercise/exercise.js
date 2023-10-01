c708cbce1da720707acbe8715e4ab9ecfd6693c9
BlueBojack <curtis.ducasse1g@gmail.com>
2023-10-01 23:42:50 +0200

    missing exercise added.

--- Esercizio_38/exercise/exercise.js
+++ Esercizio_38/exercise/exercise.js
@@ -1,1 +1,29 @@
 const isLogged = true;
+
+const logCheck = (isLogged) => {
+  return new Promise((resolve, reject) => {
+    if (isLogged) {
+      const randomNum = Math.random()
+      resolve(randomNum)
+    } else {
+      reject(new Error("You are not logged in!"))
+    }
+  })
+}
+
+const getUserInfo = (inputNumber) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (inputNumber > 0.5) {
+        resolve({ name: "John", age: 24 })
+      } else {
+        reject(new Error('Num is smaller than 0.5'))
+      }
+    })
+  })
+}
+
+logCheck(isLogged)
+  .then((randomNum) => getUserInfo(randomNum))
+  .then((user) => console.log(user))
+  .catch((err) => console.error(err))
--- Esercizio_39/exercise/exercise.js
+++ Esercizio_39/exercise/exercise.js
@@ -1,1 +1,30 @@
+const isLogged = true;
 
+const logCheck = (isLogged) => {
+  return new Promise((resolve, reject) => {
+    if (isLogged) {
+      const randomNum = Math.random()
+      resolve(randomNum)
+    } else {
+      reject(new Error("You are not logged in!"))
+    }
+  })
+}
+
+const getUserInfo = (inputNumber) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (inputNumber > 0.5) {
+        resolve({ name: "John", age: 24 })
+      } else {
+        reject(new Error("Num is smaller than 0.5"))
+      }
+    })
+  })
+}
+
+isUserLogged(isLogged)
+  .then((rand) => getUserData(rand))
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err))
+  .finally(() => console.log("Done."))
